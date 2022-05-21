90981bdb5ea12fb1f5e47861bda8a870dd481084
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-05-21 16:38:37 -0400

    Agregando conexión con Eureka

--- src/main/java/ucb/edu/bo/project/MsBooksApplication.java
+++ src/main/java/ucb/edu/bo/project/MsBooksApplication.java
@@ -2,8 +2,10 @@ package ucb.edu.bo.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsBooksApplication {
 
 	public static void main(String[] args) {
